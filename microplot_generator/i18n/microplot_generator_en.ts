9c315cce9ef9bfacb09063f4de4bc1e07f3f450e
José Fernando Menes <josemenes91@gmail.com>
2025-08-04 15:43:25 -0300

    Add files via upload

---
+++ microplot_generator/i18n/microplot_generator_en.ts
@@ -0,0 +1,178 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MicroplotGenerator</name>
+    <message>
+        <location filename="../../microplot_generator.py" line="130"/>
+        <source>&amp;Microplot Generator</source>
+        <translation>&amp;Microplot Generator</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator.py" line="117"/>
+        <source>Microplot Generator</source>
+        <translation>Microplot Generator</translation>
+    </message>
+</context>
+<context>
+    <name>MicroplotGeneratorDialog</name>
+    <message>
+        <location filename="../../microplot_generator_dialog.py" line="454"/>
+        <source>Grid successfully transformed and created!</source>
+        <translation>Grid successfully transformed and created!</translation>
+    </message>
+</context>
+<context>
+    <name>MicroplotGeneratorDialogBase</name>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="14"/>
+        <source>Create Microplots Grid</source>
+        <translation>Create Microplots Grid</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="23"/>
+        <source>Grid Parameters</source>
+        <translation>Grid Parameters</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="29"/>
+        <source>Number of Rows:</source>
+        <translation>Number of Rows:</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="42"/>
+        <source>Number of Columns:</source>
+        <translation>Number of Columns:</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="58"/>
+        <source>Plot Size</source>
+        <translation>Plot Size</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="64"/>
+        <source>Plot Length (m):</source>
+        <translation>Plot Length (m):</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="77"/>
+        <source>Plot Width (m):</source>
+        <translation>Plot Width (m):</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="93"/>
+        <source>Plot Numbering</source>
+        <translation>Plot Numbering</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="99"/>
+        <source>ID Format:</source>
+        <translation>ID Format:</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="109"/>
+        <source>Sequential</source>
+        <translation>Sequential</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="114"/>
+        <source>Serpentine</source>
+        <translation>Serpentine</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="122"/>
+        <source>Start Numbering From:</source>
+        <translation>Start Numbering From:</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="132"/>
+        <source>Bottom Left</source>
+        <translation>Bottom Left</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="137"/>
+        <source>Bottom Right</source>
+        <translation>Bottom Right</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="142"/>
+        <source>Top Left</source>
+        <translation>Top Left</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="147"/>
+        <source>Top Right</source>
+        <translation>Top Right</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="155"/>
+        <source>Initial ID (optional):</source>
+        <translation>Initial ID (optional):</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="177"/>
+        <source>Click on the map to select the 4 corner points in the following order: Top Left, Top Right, Bottom Right, Bottom Left.</source>
+        <translation>Click on the map to select the 4 corner points in the following order: Top Left, Top Right, Bottom Right, Bottom Left.</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="190"/>
+        <source>Point 1 (Top Left):</source>
+        <translation>Point 1 (Top Left):</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="203"/>
+        <source>Select</source>
+        <translation>Select</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="210"/>
+        <source>Point 2 (Top Right):</source>
+        <translation>Point 2 (Top Right):</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="223"/>
+        <source>Point 3 (Bottom Right):</source>
+        <translation>Point 3 (Bottom Right):</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="236"/>
+        <source>Point 4 (Bottom Left):</source>
+        <translation>Point 4 (Bottom Left):</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="252"/>
+        <source>Field Data (Optional)</source>
+        <translation>Field Data (Optional)</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="258"/>
+        <source>Field Map File (.csv):</source>
+        <translation>Field Map File (.csv):</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="271"/>
+        <source>Plot Data File (.csv):</source>
+        <translation>Plot Data File (.csv):</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="284"/>
+        <source>Plot ID Join Column:</source>
+        <translation>Plot ID Join Column:</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="297"/>
+        <source>Output</source>
+        <translation>Output</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="303"/>
+        <source>Output File (Optional):</source>
+        <translation>Output File (Optional):</translation>
+    </message>
+    <message>
+        <location filename="../../microplot_generator_dialog_base.ui" line="316"/>
+        <source>Create Grid</source>
+        <translation>Create Grid</translation>
+    </message>
+</context>
+</TS>
